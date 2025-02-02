dd067b64f9784e84e8530a37a8556d03e1ff2a7a
WintanaGebrehiwot <119295945+WintanaGebrehiwot@users.noreply.github.com>
2025-02-02 22:28:10 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,21 @@
-# train/urls.py
-from django.urls import path
-from .views import predict_disease
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse
 from django.conf.urls.static import static
 
+
+def home(request):
+    return HttpResponse("Welcome to the Coffee Leaf Disease Detection System!")
+
 urlpatterns = [
-    path('', predict_disease, name='predict'),
+
+   # path('api/', include('api.urls')),  # API endpoints
+    path('', home, name='home'),  # Add this line for the root URL
+    path('admin/', admin.site.urls),
+    path('predict/', include('predict.urls')),
+    path('train/', include('train.urls')),
+    #path('media/<path:path>/', some_media_view),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
